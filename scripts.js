ef0b57784dcefe7a7a528557396638f2351df38e
VanLamar <75677050+VanLamar@users.noreply.github.com>
2024-09-21 13:46:53 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function toggleMobileMenu() {
+    var mobileMenu = document.getElementById('mobileMenu');
+    if (mobileMenu.style.display === 'block') {
+        mobileMenu.style.display = 'none';
+    } else {
+        mobileMenu.style.display = 'block';
+    }
+}
